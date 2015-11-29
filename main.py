c22565a6e6d3f9d533501d34ab4b93472d631578
Sam Fuchs <samcfuchs@gmail.com>
2015-11-29 17:27:00 -0500

    Began implementing functions pause, mute, volume control

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import os
 from os import sys
 from musicplayer import MusicPlayer
 
@@ -6,18 +7,22 @@ from musicplayer import MusicPlayer
 BLACK = (0, 0, 0,)
 WHITE = (255, 255, 255)
 
+# START PYGAME
 pygame.init()
-
 # Size [width, height]
 size = [500, 700] 
-screen = pygame.display.set_mode(size)
-pygame.display.set_caption("Music Control")
+#screen = pygame.display.set_mode(size)
+#pygame.display.set_caption("Music Control")
 clock = pygame.time.Clock()
 pygame.joystick.init()
 player = MusicPlayer()
 
 # BUTTON VALUES
-
+Controller = pygame.joystick.Joystick(0)
+Controller.init()
+HAT_VAL = 0
+MUTE_BUTTON = 6
+PAUSE_BUTTON = 0
 
 # CHEX MIX
 # TODO add pygame printing
@@ -32,26 +37,43 @@ while nogo:
 	nogo = False
     if joystick_count > 1:
 	# Pick a joystick
+        print "Too many joysticks"
 
 done = False
 # MAIN GAME LOOP
 while done == False:
     # EVENT PROCESSING
-    for even in pygame.event.get():
+    for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
         if event.type == pygame.JOYBUTTONDOWN:
             print "button press"
         if event.type == pygame.JOYBUTTONUP:
             print "button release"
-
-    # DRAWING
-    screen.fill(WHITE)
-
-    # Limit to 20 fps
-    clock.tick(20)
     
-    if joystick.get_button(7) == 1:
+    # CHANGE VOLUME
+    hat = Controller.get_hat(HAT_VAL)
+    margin = "2"
+    if hat[1] == 1:
+	player.volume_adjust("+" + margin)
+    if hat[1] == -1:
+	player.volume_adjust("-" + margin)
+
+    # MUTING
+    if Controller.get_button(MUTE_BUTTON) == 1:
+	player.mute()
+
+    # PAUSING
+    if Controller.get_button(PAUSE_BUTTON) == 1:
+	player.pause()
+    
+    # DRAWING
+    #screen.fill(WHITE)
+
+    # Limit fps
+    clock.tick(10)
+    
+    if Controller.get_button(8) == 1:
         done = True
 
 # END
--- musicplayer.py
+++ musicplayer.py
@@ -4,13 +4,14 @@ import subprocess
 from song import Song
 
 class MusicPlayer:
+    '''A class to contain all the functions related to mpc'''
     def __init__(self):
-        self.path = path
-	        
-    def shuffle():
+	print "Created MusicPlayer instance"
+
+    def shuffle(self):
 	sys("mpc shuffle")
 
-    def get_now_playing():
+    def get_now_playing(self):
 	out_string = subprocess.check_output("mpc current")
 	out_list = out_string.split(" - ")
 	artist = out_list[0]
@@ -18,19 +19,27 @@ class MusicPlayer:
 
 	return Song(artist, album, song)
 	
-    def volume_adjust(value):
+    def volume_adjust(self, value):
 	'''Changes the volume by value (+-)'''
 	if value == 0:
 	    return
-        sys("mpc volume " + str(value))
+        sys("mpc volume " + value)
 
-    def volume_set(value):
+    def volume_set(self, value):
         '''Sets the volume to a value 0-100'''
 	if value == 0:
 	    return
         sys("mpc volume " + str(value))
     
-    def stop():
+    def stop(self):
 	'''Stops music'''
 	sys("mpc stop")
+    
+    def mute(self):
+	'''Mutes music'''
+	sys("mpc volume 0")
+    
+    def pause(self):
+	'''Pauses music'''
+	sys("mpc pause")
 
